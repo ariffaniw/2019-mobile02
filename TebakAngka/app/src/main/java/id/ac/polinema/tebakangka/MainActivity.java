02fba7a6f15e1a41b5ac8dd28bdfe654c2fc4a98
Fani <ariffaniwidyaningrum07@gmail.com>
2019-11-09 22:08:15 +0700

    Tebak Angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -12,59 +12,53 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-	private EditText numberText;
-	private Button guessButton, resetButton;
-	int min = 1;
-	int max = 100;
-	int randomNumber = initRandomNumber();
+	//  TODO: deklarasikan variabel di sini
+	private int Inputan,angkaRandom;
+	private Random random;
+	private EditText InputanForm;
+	private Button guessBtn;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		numberText = findViewById(R.id.number_input);
-		guessButton = findViewById(R.id.guess_button);
-		resetButton = findViewById(R.id.reset_button);
-
-		resetButton.setEnabled(false);
+		// TODO: bind layout di sini
+		InputanForm = findViewById(R.id.number_input);
+		guessBtn = findViewById(R.id.guess_button);
+		random = new Random();
+		initRandomNumber();
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
-		int random = (int) (Math.random() * ((max - min) + 1)) + min;
-		return random;
+		angkaRandom = random.nextInt (100) + 1;
 	}
 
 	public void handleGuess(View view) {
-		if (numberText.getText().toString().equals("")) {
-			Toast.makeText(this, "Isilah tebakan anda!", Toast.LENGTH_SHORT).show();
+		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		String numInputString = InputanForm.getText().toString();
+		if (numInputString.equalsIgnoreCase( "")) {
+			Toast.makeText(  this,"Input harus berupa angka",Toast.LENGTH_SHORT).show();
 		} else {
-			int guessNumber = Integer.parseInt(numberText.getText().toString());
-
-			if (guessNumber > max) {
-				Toast.makeText(this, "Tebakan anda lebih dari 100!", Toast.LENGTH_SHORT).show();
-			} else if (guessNumber < min) {
-				Toast.makeText(this, "Tebakan anda kurang dari 1!", Toast.LENGTH_SHORT).show();
-			} else if (guessNumber > randomNumber) {
-				Toast.makeText(this, "Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
-			} else if (guessNumber < randomNumber) {
-				Toast.makeText(this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
-			} else {
-				Toast.makeText(this, "Tebakan anda benar!", Toast.LENGTH_SHORT).show();
-				guessButton.setEnabled(false);
-				numberText.setEnabled(false);
-				resetButton.setEnabled(true);
+			Inputan =Integer.parseInt(numInputString);
+			if (Inputan > angkaRandom){
+				Toast.makeText( this, "Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
+			}else if(Inputan < angkaRandom){
+				Toast.makeText( this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
+			}else if(Inputan == angkaRandom){
+				Toast.makeText( this, "Tebakan anda benar", Toast.LENGTH_SHORT).show();
+				guessBtn.setEnabled(false);
 			}
 		}
+
 	}
 
 	public void handleReset(View view) {
-		numberText.setText("");
-		guessButton.setEnabled(true);
-		numberText.setEnabled(true);
-		randomNumber = initRandomNumber();
-		resetButton.setEnabled(false);
+		// TODO: Reset tampilan
+		InputanForm.setText("");
+		initRandomNumber();
+		guessBtn.setEnabled(true);
 	}
 }
 
